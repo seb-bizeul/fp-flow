b171462cda01cd9a06187f98eb1c5abf0098eb6f
Seb Bizeul <seb.bizeul@gmail.com>
2018-09-06 23:13:26 +0200

    build: rebuild lib

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import * as either from './either'
 import * as maybe from './maybe'
 import * as remoteData from './remote-data'
+import { pipe, compose } from './control-flow'
 
 export default {
   either,
   maybe,
-  remoteData
+  remoteData,
+  pipe,
+  compose
 }
