6a488c73e9a3cd66c8d676014c8ee9f69445db79
Seb Bizeul <seb.bizeul@gmail.com>
2018-10-09 08:12:11 +0200

    refactor: move pipe and compose

--- src/compose.js
+++ src/compose.js
@@ -1,7 +1,3 @@
-export const pipe = (...funs) => {
-  return x => funs.reduce((acc, f) => f(acc), x)
-}
-
 export const compose = (...funs) => {
   return x => funs.reduceRight((acc, f) => f(acc), x)
 }
